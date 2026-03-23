a33c86c6ebcc0878bceb1e8736c0b44b4ba0f225
Tanishka Singh <singhtanishka128@gmail.com>
2026-03-23 15:07:13 +0530

    testing h2 database

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,6 +1,18 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="categories")
 public class Category {
+
+    public Category() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
