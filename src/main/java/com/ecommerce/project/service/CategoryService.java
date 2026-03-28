782aee696dc536160a05ae479d9118b211b055fa
Tanishka Singh <singhtanishka128@gmail.com>
2026-03-28 20:01:04 +0530

    implementing dto to createCategory

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,14 +1,12 @@
 package com.ecommerce.project.service;
 
-
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 
-import java.util.List;
 
 public interface CategoryService {
     CategoryResponse getAllCategories();
-    void createCategory(Category category);
-    String deleteCategory(Long categoryId);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);    String deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
 }
