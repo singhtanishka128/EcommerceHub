7b95b8663071000269a552327553c0ff1546eda6
Tanishka Singh <singhtanishka128@gmail.com>
2026-04-04 00:15:54 +0530

    update product image

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, ProductDTO product);
@@ -11,4 +13,5 @@ public interface ProductService {
     ProductResponse searchProductByKeyword(String keyword);
     ProductDTO updateProduct(Long productId, ProductDTO product);
     ProductDTO deleteProduct(Long productId);
+    ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
 }
