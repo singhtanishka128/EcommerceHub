955b53ae61add0586180d38171ed63de4ab748f8
Tanishka Singh <singhtanishka128@gmail.com>
2026-04-03 19:29:07 +0530

    get all products

--- src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -12,7 +12,7 @@ public class ProductDTO {
     private String productName;
     private String image;
     private Integer quantity;
-    private double price;
-    private double discount;
-    private double specialPrice;
+    private Double price;
+    private Double discount;
+    private Double specialPrice;
 }
