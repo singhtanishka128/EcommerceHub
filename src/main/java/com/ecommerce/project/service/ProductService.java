3d97e7775fa4ab5c3f33b565b529dd1248bf5c36
Tanishka Singh <singhtanishka128@gmail.com>
2026-04-03 20:34:01 +0530

    get products by keyword

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -8,4 +8,5 @@ public interface ProductService {
     ProductDTO addProduct(Long categoryId, Product product);
     ProductResponse getAllProducts();
     ProductResponse searchByCategory(Long categoryId);
+    ProductResponse searchProductByKeyword(String keyword);
 }
