fa48b75a411460fdfec345465c73521abdb5bd6e
Tanishka Singh <singhtanishka128@gmail.com>
2026-04-24 18:42:38 +0530

    deleting address

--- src/main/java/com/ecommerce/project/service/AddressService.java
+++ src/main/java/com/ecommerce/project/service/AddressService.java
@@ -10,4 +10,5 @@ public interface AddressService {
     AddressDTO getAddressesById(Long addressId);
     List<AddressDTO> getUserAddresses(User user);
     AddressDTO updateAddress(Long addressId, AddressDTO addressDTO);
+    String deleteAddress(Long addressId);
 }
