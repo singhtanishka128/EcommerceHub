a49241a319e0ce1fd5db38f3a5d595a99994629c
Tanishka Singh <singhtanishka128@gmail.com>
2026-04-24 18:36:20 +0530

    get address by user

--- src/main/java/com/ecommerce/project/service/AddressService.java
+++ src/main/java/com/ecommerce/project/service/AddressService.java
@@ -8,4 +8,5 @@ public interface AddressService {
     AddressDTO createAddress(AddressDTO addressDTO, User user);
     List<AddressDTO> getAddresses();
     AddressDTO getAddressesById(Long addressId);
+    List<AddressDTO> getUserAddresses(User user);
 }
